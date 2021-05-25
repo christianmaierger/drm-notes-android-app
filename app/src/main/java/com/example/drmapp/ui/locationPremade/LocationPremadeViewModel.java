02fd7c09ea4d39d1aaa04f1059dc0da1771fe76c
Felix Schenke <felix.schenke@stud.uni-bamberg.de>
2021-05-25 12:46:11 +0200

    added location selection menu between people and overall mood selection; adjusted progress bar accordingly in all other menues

---
+++ app/src/main/java/com/example/drmapp/ui/locationPremade/LocationPremadeViewModel.java
@@ -0,0 +1,7 @@
+package com.example.drmapp.ui.locationPremade;
+
+import androidx.lifecycle.ViewModel;
+
+public class LocationPremadeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
