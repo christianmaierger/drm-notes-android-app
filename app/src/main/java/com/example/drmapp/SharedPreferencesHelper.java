f3f05cfe5363d815000503fe8c75177924379092
Christian Maier <christian1.maier@stud.uni-bamberg.de>
2021-06-09 12:24:28 +0200

    start implementing storing little stuff for gui and alarms in shared prefferences

--- app/src/main/java/com/example/drmapp/SharedPreferencesHelper.java
+++ app/src/main/java/com/example/drmapp/SharedPreferencesHelper.java
@@ -22,5 +22,22 @@ public class SharedPreferencesHelper {
 
     }
 
+
+
+    public static String getNotificationTime1AsString(Context context, Integer timeButtonNumber) {
+        // Die timeButtonNumber fungiert hier als key zu dem jeweiligen Value einer Alarm Zeit im
+        // SharedPreferencesFile
+        return context.getSharedPreferences("PREF_NAME_NOTIFICATIONASSTRING", Context.MODE_PRIVATE)
+                .getString(timeButtonNumber.toString(), null);
+    }
+
+    public static void saveNotification1AsString(Context context, Integer timeButtonNumber, String time) {
+        context.getSharedPreferences("PREF_NAME_NOTIFICATION", Context.MODE_PRIVATE)
+                .edit().putString(timeButtonNumber.toString(), time).apply();
+
+    }
+
+
+
 }
 
